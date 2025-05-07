cf9565d13bc54c094db1e2994927ec1bb98beb8b
franccium <bawercx@gmail.com>
2025-05-07 20:14:21 +0200

    Add project files.

---
+++ MofuEngine/Core/main.cpp
@@ -0,0 +1,31 @@
+
+#if _WIN64
+#ifndef WIN32_MEAN_AND_LEAN
+#define WIN32_MEAN_AND_LEAN
+#endif
+
+#include <Windows.h>
+#include <crtdbg.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+#if _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+	MSG msg;
+	bool is_running{ true };
+	while (is_running)
+	{
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			is_running &= (msg.message != WM_QUIT);
+		}
+		// Update
+	}
+	// Shutdown
+	return 0;
+}
+
+#endif
